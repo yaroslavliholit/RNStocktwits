cef05ef6b5ab2e8394963b1432e367ae07796dc2
yaroslavliholit <yaroslavliholiti@gmail.com>
2021-07-05 22:28:31 +0300

    Added new api paths

--- src/shared/constants/apiPaths.ts
+++ src/shared/constants/apiPaths.ts
@@ -2,6 +2,27 @@ const apiPaths = {
   reference: {
     tickers: '/v3/reference/tickers',
   },
+  meta: {
+    symbols: (ticker: string) => `/v1/meta/symbols/${ticker}/company`,
+  },
+  dailyOpenClose: ({ticker, date}: {ticker: string; date: string}) =>
+    `/v1/open-close/${ticker}/${date}`,
+  stocks: {
+    aggregates: ({
+      stocksTicker,
+      from,
+      to,
+      multiplier = 1,
+      timespan = 'day',
+    }: {
+      stocksTicker: string;
+      from: string;
+      to: string;
+      multiplier?: number;
+      timespan?: string;
+    }) =>
+      `/v2/aggs/ticker/${stocksTicker}/range/${multiplier}/${timespan}/${from}/${to}`,
+  },
 };
 
 export default apiPaths;
