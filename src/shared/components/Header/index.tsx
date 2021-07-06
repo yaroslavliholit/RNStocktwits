9d3a931161058e7b664bee273ad211b010d91d0b
yaroslavliholit <yaroslavliholiti@gmail.com>
2021-07-06 21:47:47 +0300

    Fix Header component

--- src/shared/components/Header/index.tsx
+++ src/shared/components/Header/index.tsx
@@ -1,66 +1,37 @@
-import React, {memo, useCallback} from 'react';
-import {useNavigation} from '@react-navigation/native';
+import React, {memo} from 'react';
+import {useTheme} from 'styled-components';
 
-import {useSearchTickers} from '../../../store/search/hooks';
 import useDrawerNavigation from '../../hocs/useDrawerNavigation';
 import useTrackInputFocus from '../../hooks/useTrackInputFocus';
+import SearchWithSuggestions from '../../containers/SearchWithSuggestions';
 import Icon from '../Icon';
-import InputField from '../InputField';
-import SuggestionsList from '../SuggestionsList';
 import * as S from './styles';
-
-// TODO: add separate Search component
 
 const Header = () => {
   // region ********** DATA **********
-  const {navigate} = useNavigation();
+
+  const {colors} = useTheme();
   const {isFocus, getTrackInputFocusProps} = useTrackInputFocus();
   const {openDrawer} = useDrawerNavigation();
-  const {suggestions, getSearchTickersProps, handleClearSuggestions} =
-    useSearchTickers();
-  const isSuggestionsExist = Boolean(suggestions && suggestions.length);
-  // endregion
-
-  // region ********** CALLBACKS **********
-  const handleSelectSearchItem = useCallback(
-    ({ticker}: Ticker) => {
-      navigate('CompanyDetails', {
-        ticker,
-      });
-
-      handleClearSuggestions();
-    },
-    [navigate, handleClearSuggestions],
-  );
   // endregion
 
   // region ********** JSX **********
   return (
-    <S.Header withSearchResults={isSuggestionsExist}>
-      <S.Container>
-        {!isFocus && (
-          <S.MenuButton onPress={openDrawer}>
-            <Icon type={'menu'} width={15} height={15} fill={'#000000'} />
-          </S.MenuButton>
-        )}
-        <S.SearchArea>
-          <InputField
-            {...getTrackInputFocusProps()}
-            {...getSearchTickersProps()}
-            placeholder={'Search symbols or companies'}
-            renderLeftIcon={
-              <Icon type={'search'} width={15} height={15} fill={'#000000'} />
-            }
+    <S.Container>
+      {!isFocus && (
+        <S.MenuButton onPress={openDrawer}>
+          <Icon
+            type={'menu'}
+            width={15}
+            height={15}
+            fill={colors.ui.secondary}
           />
-        </S.SearchArea>
-      </S.Container>
-      {suggestions && (
-        <SuggestionsList
-          items={suggestions}
-          onItemSelect={handleSelectSearchItem}
-        />
+        </S.MenuButton>
       )}
-    </S.Header>
+      <S.SearchArea isSearchFocused={isFocus}>
+        <SearchWithSuggestions {...getTrackInputFocusProps()} />
+      </S.SearchArea>
+    </S.Container>
   );
   // endregion
 };
