7703cfa0822eef3a0a321e050a1395f64d01c43d
yaroslavliholit <yaroslavliholiti@gmail.com>
2021-07-10 16:41:48 +0300

    Minor fixes

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,6 @@
 import React, {memo} from 'react';
 import {SafeAreaView} from 'react-native';
+
 import Header from '../../shared/components/Header';
 
 const Home = () => {
--- src/shared/constants/env.ts
+++ src/shared/constants/env.ts
@@ -3,7 +3,7 @@ import Config from 'react-native-config';
 const ENV = {
   LOAD_STORYBOOK: Config.LOAD_STORYBOOK,
   POLYGON_API_BASE_URL: Config.POLYGON_API_BASE_URL,
-  POLYGON_API_KEY: Config.POLYGON_API_KEY
+  POLYGON_API_KEY: Config.POLYGON_API_KEY,
 };
 
 export default ENV;
