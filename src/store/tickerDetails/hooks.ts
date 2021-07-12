f62bfbf2195dabeb3751fd6d4ef1bd4097241cae
yaroslavliholit <yaroslavliholiti@gmail.com>
2021-07-12 20:56:33 +0300

    Changed app icon and name

--- src/store/tickerDetails/hooks.ts
+++ src/store/tickerDetails/hooks.ts
@@ -89,7 +89,7 @@ export const useFetchTickerDetails = (ticker: string) => {
       dispatch(
         fetchDailyOpenCloseData({
           ticker: value,
-          date: getFormatDate({extraDay: -1}),
+          date: getFormatDate({extraDay: -3}),
           closeDate: 'current',
         }),
       );
@@ -97,7 +97,7 @@ export const useFetchTickerDetails = (ticker: string) => {
       dispatch(
         fetchDailyOpenCloseData({
           ticker: value,
-          date: getFormatDate({extraDay: -2}),
+          date: getFormatDate({extraDay: -4}),
           closeDate: 'previous',
         }),
       );
