47f0b55d4a6a18af42f3f373371bd01f07dac09f
yaroslavliholit <yaroslavliholiti@gmail.com>
2021-07-12 21:34:33 +0300

    Added content to AboutUs screen

--- src/screens/AboutUs/index.tsx
+++ src/screens/AboutUs/index.tsx
@@ -1,10 +1,45 @@
-import React, {memo} from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, {memo, useCallback} from 'react';
+import {SafeAreaView} from 'react-native';
+import {Linking} from 'react-native';
 
-const AboutUs = () => (
-  <SafeAreaView>
-    <Text>About Us</Text>
-  </SafeAreaView>
-);
+import Typography from '../../shared/components/Typography';
+import Spacer from '../../shared/components/Spacer';
+
+const AboutUs = () => {
+  const handleFreePikLink = useCallback(() => {
+    Linking.openURL('https://www.freepik.com');
+  }, []);
+
+  const handleFlatIconLink = useCallback(() => {
+    Linking.openURL('https://www.flaticon.com/');
+  }, []);
+
+  const handleOpenStreetMapLink = useCallback(() => {
+    Linking.openURL('https://osm.org/copyright');
+  }, []);
+
+  return (
+    <SafeAreaView>
+      <Spacer positionType={['left', 'right']} sizeType={'medium'}>
+        <Spacer positionType={'bottom'} sizeType={'medium'}>
+          <Typography variant={'subtitle'}>Icons made by</Typography>
+          <Typography variant={'link'} onPress={handleFreePikLink}>
+            Freepik
+          </Typography>
+        </Spacer>
+        <Spacer positionType={'bottom'} sizeType={'medium'}>
+          <Typography variant={'subtitle'}>from</Typography>
+          <Typography variant={'link'} onPress={handleFlatIconLink}>
+            www.flaticon.com
+          </Typography>
+        </Spacer>
+        <Typography variant={'subtitle'}>Geocoding</Typography>
+        <Typography variant={'link'} onPress={handleOpenStreetMapLink}>
+          © OpenStreetMap contributors
+        </Typography>
+      </Spacer>
+    </SafeAreaView>
+  );
+};
 
 export default memo(AboutUs);
